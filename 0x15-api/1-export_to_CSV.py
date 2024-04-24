8e852cf1af2577553603199bf47b168933f7eaa7
creeds-knight <apedoarthur21@gmail.com>
2024-04-24 15:30:25 +0300

    task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -26,12 +26,9 @@ def employee_data(emp_id):
     # export to csv
     filename = f"{emp_id}.csv"
     with open(filename, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                        "TASK_TITLE"])
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for todo in todo_data:
-            writer.writerow([str(emp_id), str(name), str(todo['completed']),
-                            str(todo['title'])])
+            writer.writerow([emp_id, name, todo['completed'], todo['title']])
 
 
 if __name__ == "__main__":

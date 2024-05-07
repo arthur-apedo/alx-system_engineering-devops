c812fad72d194e1751fa8140ebe519e67ef9f487
creeds-knight <apedoarthur21@gmail.com>
2024-05-07 15:23:10 +0300

    task-0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" A script to return the total number of subscribers in a subreddit """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ returns the number of subscribers in a subreddit """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-agent': 'Bot/0.1'}
+
+    response = requests.get(url, headers=headers)
+    try:
+        if response.status_code == 200:
+            all_data = response.json()
+            if "data" in all_data and "subscribers" in all_data['data']:
+                subscribers = all_data['data']['subscribers']
+                return subscribers
+        else:
+            return 0
+    except Exception as e:
+        print(e)
